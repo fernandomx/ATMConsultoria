2a6c01ba12f3fbd9ecce7c4e9f51e5cbc578a6d6
fernandomx <fernando973@gmail.com>
2020-05-02 15:24:50 -0300

    Star commit!

---
+++ app/src/main/java/com/example/navigationdrawer/ui/sobre/SobreFragment.java
@@ -0,0 +1,29 @@
+package com.example.navigationdrawer.ui.sobre;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.navigationdrawer.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SobreFragment extends Fragment {
+
+    public SobreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sobre, container, false);
+    }
+}
